a872dac3e7c462e6b4c40cea88e788b51907ad9c
shiven16 <145124527+shiven16@users.noreply.github.com>
2024-08-18 20:57:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.post("/file/create", (req, res) => {
 
 app.put(`/file/:fileName`, (req, res) => {
   const {fileName} = req.params;
-  const {updatedFileName, fileData} = req.body;
+  const {updatedFileName, newFileData} = req.body;
   updateAfile(fileName, updatedFileName, newFileData)
   res.status(200).send({message: "File updated successfully"});
 })
